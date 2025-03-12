32ae50e9b08676f95c819ad33d13721a5aa5149d
ela0209 <elakaracay2005@gmail.com>
2025-03-12 20:37:49 +0300

    odev yapildi

---
+++ NameSpace/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NameSpace;
+
+Console.WriteLine("Hello, World!");
+Hesaplama hesaplama = new Hesaplama();
+hesaplama.Topla(3, 5);
+hesaplama.Carp(3, 5);
